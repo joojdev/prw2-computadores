eacb5e9c507a3774ff80eee4855dc7bd2187386b
João Vitor Piovezan <joao.piovezan@grupomultiplica.com.br>
2026-03-16 19:07:04 +0000

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import '../styles/Hero.css'
+
+function Hero() {
+  return (
+    <>
+      <div id="hero">
+        <div className="filter">
+          <div className="text">
+            <h1>Eles da Silva Informática</h1>
+            <p>Onde a sua satisfação é o dever deles ☝️</p>
+            <button>Conheça eles</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Hero
